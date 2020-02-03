1dc45a7e450ab478d5cd39a02c6efbe24d5f7e5d
masaya <spag9hc9@yahoo.co.jp>
2020-02-03 22:41:39 +0900

    確認用

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import { msg, msgAlert } from './lib.js'
-
-console.log(msg)
-msgAlert()
-
-import './main.scss';
--- src/lib.js
+++
@@ -1,6 +0,0 @@
-let msg = "masaya"
-let msgAlert = () => {
-    alert(msg + " is nice guy")
-}
-
-export { msg, msgAlert }
